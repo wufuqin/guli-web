0b26bc47d506544118a4b87529a82d6b9760efcc
wufuqin <1820191171@qq.com>
2021-02-25 23:07:52 +0800

    完成了课程发布功能，但是目前修改课程章节功能和修改小节功能没有完成

---
+++ src/api/edu/video.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+export default {
+
+  /**
+   * 添加小节
+   * @param video
+   */
+  addVideo(video) {
+        return request({
+            url: '/eduservice/video/addVideo',
+            method: 'post',
+            data: video
+          })
+    },
+
+  /**
+   * 删除小节
+   * @param id
+   */
+  deleteVideo(id) {
+        return request({
+            url: '/eduservice/video/'+id,
+            method: 'delete'
+          })
+    },
+}
--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container">
+    讲师表单
     <el-form label-width="120px">
       <el-form-item label="讲师名称">
         <el-input v-model="teacher.name"/>
@@ -21,6 +22,33 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+    v-show：是否显示上传组件
+    :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+    :url：后台上传的url地址
+    @close：关闭上传组件
+    @crop-upload-success：上传成功后的回调
+      <input type="file" name="file"/>
+    -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/eduoss/fileoss'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
 
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
@@ -31,11 +59,14 @@
 </template>
 <script>
   import teacherApi from '@/api/edu/teacher'
+  import ImageCropper from '@/components/ImageCropper'
+  import PanThumb from '@/components/PanThumb'
 
   export default {
+    components: { ImageCropper, PanThumb },
     data() {
       return {
-        teacher: {
+        teacher:{
           name: '',
           sort: 0,
           level: 1,
@@ -43,7 +74,12 @@
           intro: '',
           avatar: ''
         },
-        saveBtnDisabled: false  // 保存按钮是否禁用,
+
+        //上传弹框组件是否显示
+        imagecropperShow:false,
+        imagecropperKey:0,//上传组件key值
+        BASE_API:process.env.BASE_API, //获取dev.env.js里面地址
+        saveBtnDisabled:false  // 保存按钮是否禁用,
       }
     },
     created() { //页面渲染之前执行
@@ -54,10 +90,22 @@
         this.init()
       }
     },
-    methods: {
+    methods:{
+      close() { //关闭上传弹框的方法
+        this.imagecropperShow=false
+        //上传组件初始化
+        this.imagecropperKey = this.imagecropperKey+1
+      },
+      //上传成功方法
+      cropSuccess(data) {
+        this.imagecropperShow=false
+        //上传之后接口返回图片地址
+        this.teacher.avatar = data.url
+        this.imagecropperKey = this.imagecropperKey+1
+      },
       init() {
         //判断路径有id值,做修改
-        if (this.$route.params && this.$route.params.id) {
+        if(this.$route.params && this.$route.params.id) {
           //从路径获取id值
           const id = this.$route.params.id
           //调用根据id查询的方法
@@ -77,7 +125,7 @@
       saveOrUpdate() {
         //判断修改还是添加
         //根据teacher是否有id
-        if (!this.teacher.id) {
+        if(!this.teacher.id) {
           //添加
           this.saveTeacher()
         } else {
@@ -93,12 +141,11 @@
             this.$message({
               type: 'success',
               message: '修改成功!'
-            })
+            });
             //回到列表页面 路由跳转
-            this.$router.push({ path: '/teacher/table' })
+            this.$router.push({path:'/teacher/table'})
           })
       },
-
       //添加讲师的方法
       saveTeacher() {
         teacherApi.addTeacher(this.teacher)
@@ -107,9 +154,9 @@
             this.$message({
               type: 'success',
               message: '添加成功!'
-            })
+            });
             //回到列表页面 路由跳转
-            this.$router.push({ path: '/teacher/table' })
+            this.$router.push({path:'/teacher/table'})
           })
       }
 
